d69f69830328d603d0e025840ea20c4cc261dd9c
Aadarshbhushan-Singh <singhaadarsh5050@gmail.com>
2022-01-23 21:12:27 +0530

    Updates

---
+++ precache-manifest.63e745851e772734500f5d0edc84878e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ba118a603acf8ed4005d6928561032",
+    "url": "/getUpdated/index.html"
+  },
+  {
+    "revision": "dc702a7b4c37c46c4551",
+    "url": "/getUpdated/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "601093bd212ec076299d",
+    "url": "/getUpdated/static/css/main.c61be1c4.chunk.css"
+  },
+  {
+    "revision": "dc702a7b4c37c46c4551",
+    "url": "/getUpdated/static/js/2.1dcc757b.chunk.js"
+  },
+  {
+    "revision": "601093bd212ec076299d",
+    "url": "/getUpdated/static/js/main.bccd8cc5.chunk.js"
+  },
+  {
+    "revision": "84ac5e4274d2f196f8c8",
+    "url": "/getUpdated/static/js/runtime~main.7706780f.js"
+  }
+]);
