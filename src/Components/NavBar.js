9af1eca09c55a602415c921aacb4648c76ba88a2
Aadarshbhushan-Singh <singhaadarsh5050@gmail.com>
2022-01-23 21:01:26 +0530

    Updating files

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -8,7 +8,7 @@ export default class NavBar extends Component {
             <div>
                 <Navbar bg="light" expand="lg">
                     <Container>
-                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                        <Navbar.Brand href="#home">Get Updated</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
